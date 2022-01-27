2f7684ca368c6059d2ff0194609f94e28375e5ce
vvpyankov@mail.ru <vvpyankov@mail.ru>
2022-01-27 22:30:51 +0300

    27/01/2022

--- lab13/Program.cs
+++ lab13/Program.cs
@@ -10,6 +10,55 @@ namespace lab13
     {
         static void Main(string[] args)
         {
+            /*//Triangle triangle = new Triangle();
+            RectangularTriangle rectangularTriangle = new RectangularTriangle(4,5);
+            Triangle triangle = rectangularTriangle; //upcasting
+            RectangularTriangle rectangularTriangle1 = (RectangularTriangle) triangle; //downcasting*/
+            RegularTriangle regularTriangle = new RegularTriangle();
+            Triangle triangle = regularTriangle;
+            /*RectangularTriangle rectangularTriangle = triangle as RectangularTriangle;*/
+            if (triangle is RectangularTriangle)
+            {
+                RectangularTriangle rectangularTriangle = (RectangularTriangle)triangle;
+            }
+            Console.ReadKey();
+
         }
+    }
+    class Triangle
+    {
+        public double A { get; set; }
+        public double B { get; set; }
+        public double C { get; set; }
+        public Triangle()
+        {
+
+        }
+        public Triangle(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+        public double GetArea()
+        {
+            double p = (A + B + C) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+
+        }
+    }
+    class RectangularTriangle : Triangle
+    {
+        public RectangularTriangle(double a, double b)
+        //  : base(a, b, Math.Sqrt(a * a + b * b)) - вызов родительского конструктора
+        {
+            A = a;
+            B = b;
+            C = Math.Sqrt(a * a + b * b);
+        }
+    }
+    class RegularTriangle:Triangle
+    {
+
     }
 }
---
+++ lab13zadacha/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*Задан класс Building, который описывает здание. Класс содержит следующие элементы:
+
+адрес здания;
+длина здания;
+ширина здания;
+высота здания.
+В классе Building нужно реализовать следующие методы:
+
+конструктор с 4 параметрами;
+свойства get/set для доступа к полям класса;
+метод Print(), который выводит информацию о здании.
+Разработать класс MultiBuilding, который наследует возможности класса Building и добавляет поле этажность. В классе MultiBuilding реализовать следующие элементы:
+
+конструктор с 5 параметрами – реализует вызов конструктора базового класса;
+свойство get/set доступа к внутреннему полю класса;
+метод Print(), который обращается к методу Print() базового класса Building для вывода информации о всех полях класса.
+Класс MultiBuilding сделать таким, что не может быть унаследован.*/
+
+namespace lab13zadacha
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Building building = new Building("Университетский пр-т, дом 3", 40, 30, 20);
+            building.Print();
+            MultiBuilding multiBuilding = new MultiBuilding("Университетский пр-т, дом 3", 40, 30, 20, 5);
+            multiBuilding.Print();
+        }
+
+    }
+    public class Building //Задан класс Building, который описывает здание. Класс содержит след эл-ты:адр; дл; шир; выс
+
+    {
+        protected string adress;
+        protected int length;
+        protected int wide;
+        protected int heigh;
+
+        public Building(string Adress, int length, int wide, int heigh) // метод Print(), который выводит информацию о здании.
+        {
+            this.Adress = Adress;
+            this.length = length;
+            this.wide = wide;
+            this.heigh = heigh;
+        }
+        public string Adress
+        {
+            get { return adress; }
+            set { adress = value; }
+        }
+        public int Length
+        {
+            get { return length; }
+            set { length = value; }
+        }
+        public int Wide
+        {
+            get { return wide; }
+            set { wide = value; }
+        }
+        public int Heigh
+        {
+            get { return heigh; }
+            set { heigh = value; }
+        }
+        public void Print(/*string adress, int length, int wide, int heigh*/)
+        {
+            Console.WriteLine("Адрес здания {0}, длина {1} м, ширина {2} м,высота {3} м.", adress, length, wide, heigh);
+            Console.ReadKey();
+        }
+
+
+    }
+    sealed class MultiBuilding : Building //дочерний класс
+    {
+        protected int floor { get; set; }
+        public MultiBuilding(string adress, int length, int wide, int heigh, int floor)
+                        : base(adress, length, wide, heigh)
+        {
+            base.Adress = Adress;
+            this.length = length;
+            this.wide = wide;
+            this.heigh = heigh;
+            this.Floor = floor;
+        }
+        public int Floor
+        {
+            get { return floor; }
+            set { floor = value; }
+        }
+        public new void Print()
+        {
+            base.Print();
+            Console.WriteLine("Количество этажей:{0}", floor);
+            Console.ReadKey();
+        }
+    }
+}
+
+
+
